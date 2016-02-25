2472c5157e4050469793997e11579e92efc96872
libertysyarn <code@libertysyarn.com>
2016-02-25 17:55:51 -0500

    more api

--- js/index.js
+++ js/index.js
@@ -1,42 +1,29 @@
-$(document).ready(function() {
-  reqListener();
+// var game = (status !== null) ? status : '';
+// var status = (status === null) ? "mdl-color--blue-grey-50" : "mdl-color-text--purple";
+// var tvUrl = "http://twitch.tv/" + name;
+//   $.getJSON('https://api.twitch.tv/kraken/streams?channel=' + users + '&callback=jsonp', function(news) {
+//var url = 'https://api.twitch.tv/kraken/streams?channel=' + users + '&callback=jsonp';
+//'freecodecamp,storbeck,terakilobyte,habathcx,RobotCaleb,thomasballinger,noobs2ninjas,beohoff,NoCopyrightSounds'
 
-  function reqListener() {
-    var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "NoCopyrightSounds"];
-    $.getJSON("https://api.twitch.tv/kraken/users/" + users[i] + "?callback=?", function(results) {
-      var data = JSON.stringify(results);
-      var twitch = '';
-      var bio = data.bio;
-      var game = (data.status !== null) ? data.status : '';
-      var logo = data.logo;
-      var name = data.display_name;
-      var status = (data.status === null) ? "mdl-color--blue-grey-50" : "mdl-color-text--purple";
-      var url = "http://twitch.tv/" + name;
+var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "NoCopyrightSounds"];
+var names = users.join(',').toLowerCase();
+var url = 'https://api.twitch.tv/kraken/streams?channel=' + names + '&callback=jsonp';
 
-      for (var i = 0; i < users.length; i++) {
-        twitch += '<li class="mdl-list__item mdl-list__item--three-line"><a href="' + url[i] + '"<span class="mdl-list__item-primary-content"><i src="' + logo[i] + '" class="material-icons mdl-list__item-avatar"></i></a>';
-        twitch += '<span>' + name[i] + '</span><span class="mdl-list__item-text-body"><p>' + game[i] + '</p>';
-        twitch += '"</span></span><span class="mdl-list__item-secondary-content"><a class="mdl-list__item-secondary-action" href="' + url[i] + '"></a><i class="material-icons' + status[i] + '">star</i></span></li>"';
-      }
-      content = document.getElementById('list');
-      content.innerHTML = twitch;
-    })
+
+$.ajax({
+  url: 'https://api.twitch.tv/kraken/streams?',
+  data: names,
+  dataType: 'jsonp',
+  success: function(data) {
+    var twitch = '';
+    console.log(data);
+    $.map(data, function(f) {
+
+      twitch += '<li class="mdl-list__item mdl-list__item--three-line"><a href="http://twitch.tv/' + f.streams[i].name + '"<span class="mdl-list__item-primary-content"><i src="' + f.streams[i].channel.logo + '" class="material-icons mdl-list__item-avatar"></i></a>';
+      twitch += '<span>' + f.streams[i].channel.name + '</span><span class="mdl-list__item-text-body"><p>' + f.streams[i].game + '</p>';
+      twitch += '"</span></span><span class="mdl-list__item-secondary-content"><a class="mdl-list__item-secondary-action" href="' + f.streams[i].name + '"></a><i class="material-icons' + f.streams[i].channel.game + '">star</i></span></li>"';
+    });
+    var content = document.getElementById('list');
+    content.innerHTML = twitch;
   }
-
 });
-
-//   function reqListener() {
-//
-//     if (status === 200) {
-//       var twitch = '';
-//       for (var i = 0; i < 1; i++) {
-//
-//         twitch += '<li class="mdl-list__item mdl-list__item--three-line"><a href="' + url[i] + '"<span class="mdl-list__item-primary-content"><i src="' + logo[i] + '" class="material-icons mdl-list__item-avatar"></i></a>';
-//         twitch += '<span>' + name[i] + '</span><span class="mdl-list__item-text-body"><p>' + game[i] + '</p>';
-//         twitch += '"</span></span><span class="mdl-list__item-secondary-content"><a class="mdl-list__item-secondary-action" href="' + url[i] + '"></a><i class="material-icons' + status[i] + '">star</i></span></li>"';
-//       }
-//       content = document.getElementById('list');
-//       content.innerHTML = twitch;
-//     }
-//   }
-// });
