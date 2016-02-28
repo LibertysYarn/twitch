a92ebf53353d36975d8f995965c687f03afaafa5
libertysyarn <code@libertysyarn.com>
2016-02-27 21:54:30 -0500

    redo

--- js/index.js
+++ js/index.js
@@ -1,29 +1,36 @@
-// var game = (status !== null) ? status : '';
-// var status = (status === null) ? "mdl-color--blue-grey-50" : "mdl-color-text--purple";
-// var tvUrl = "http://twitch.tv/" + name;
-//   $.getJSON('https://api.twitch.tv/kraken/streams?channel=' + users + '&callback=jsonp', function(news) {
-//var url = 'https://api.twitch.tv/kraken/streams?channel=' + users + '&callback=jsonp';
-//'freecodecamp,storbeck,terakilobyte,habathcx,RobotCaleb,thomasballinger,noobs2ninjas,beohoff,NoCopyrightSounds'
-
 var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "NoCopyrightSounds"];
 var names = users.join(',').toLowerCase();
-var url = 'https://api.twitch.tv/kraken/streams?channel=' + names + '&callback=jsonp';
 
 
-$.ajax({
-  url: 'https://api.twitch.tv/kraken/streams?',
-  data: names,
-  dataType: 'jsonp',
-  success: function(data) {
-    var twitch = '';
-    console.log(data);
-    $.map(data, function(f) {
+for (var i = 0; i < users.length; i++) {
 
-      twitch += '<li class="mdl-list__item mdl-list__item--three-line"><a href="http://twitch.tv/' + f.streams[i].name + '"<span class="mdl-list__item-primary-content"><i src="' + f.streams[i].channel.logo + '" class="material-icons mdl-list__item-avatar"></i></a>';
-      twitch += '<span>' + f.streams[i].channel.name + '</span><span class="mdl-list__item-text-body"><p>' + f.streams[i].game + '</p>';
-      twitch += '"</span></span><span class="mdl-list__item-secondary-content"><a class="mdl-list__item-secondary-action" href="' + f.streams[i].name + '"></a><i class="material-icons' + f.streams[i].channel.game + '">star</i></span></li>"';
-    });
+  var urlSt = 'https://api.twitch.tv/kraken/streams?channel=' + names + '&callback=?';
+  var urlUs = 'https://api.twitch.tv/kraken/users/' + users[i] + '?callback=?';
+
+  $.getJSON(urlUs, function(u) {
+    var displayName = u.display_name;
+    var logo = u.logo;
+    var url = u._links.self;
+    var bio = u.bio;
+    console.log(u);
+
+    $.getJSON(urlSt, function(s) {
+      for (i in s) {
+        if (s.streams !== null) {
+          var game = s.streams[i].channel.game;
+          var gameTitle = (s.streams[i] !== null) ? game : '';
+          var status = (data.streams[i] === null) ? "mdl-color--blue-grey-50" : "mdl-color-text--purple";
+          console.log(s);
+        }
+      }
+
+      twitch = '';
+      twitch += '<li class="mdl-list__item mdl-list__item--three-line"><a href="' + url[i] + '"<span class="mdl-list__item-primary-content"><i src="' + logo[i] + '" class="material-icons mdl-list__item-avatar"></i></a>';
+      twitch += '<span>' + displayName[i] + '</span><span class="mdl-list__item-text-body"><p>' + bio[i] + '</p><p class="gameTitle">' + gameTitle[i] + '</p>';
+      twitch += '"</span></span><span class="mdl-list__item-secondary-content"><a class="mdl-list__item-secondary-action" href="' + url[i] + '"></a><i id="star"  class="material-icons' + status[i] + '">star</i></span></li>"';
+    })
     var content = document.getElementById('list');
     content.innerHTML = twitch;
-  }
-});
+
+  });
+}
---
+++ todo.md
@@ -0,0 +1,28 @@
+
+##### End Project
++ chat like UI with a list of user info
++ user info
+  - status
+  - avatar
+  - link to channel
+  - if active, description of broadcast
++ tab with a search box to lookup users
++ tabs that filter and display online and offline users  
+
+
+___
+#### Rubber-Duck
+ 1. call api -> "users" for each
+ 1. call api -> "streams"
+ 1. store streams status in array
+ 1. if no image, add default
+ 1. display name and bio
+ 1. if !null, run
+ 1. default state is offline
+ 1. if online, display "game title"
+ 1. if online, change icon color
+ 1. button listener for each tab
+ 1. assign online/offline to appropriate tabs
+ 1. map filtered data to this tab list component
+ 1. button listener for search tab
+ 1. search box to call or sort usernames (?)
