7073457e8f60a1cc70a60f3328f53fbfdeca6e83
libertysyarn <code@libertysyarn.com>
2016-02-28 20:19:26 -0500

    logic fix

--- js/index.js
+++ js/index.js
@@ -1,37 +1,47 @@
 var users = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "NoCopyrightSounds"];
-var usersToString = users.join(',').toLowerCase();
-
+twitch = '';
+var gameTitle = '';
 
 for (var i = 0; i < users.length; i++) {
-
-  var urlSt = 'https://api.twitch.tv/kraken/streams?channel=' + usersToString + '&callback=?';
+  var urlSt = 'https://api.twitch.tv/kraken/streams?channel=' + users[i] + '&callback=?';
   var urlUs = 'https://api.twitch.tv/kraken/users/' + users[i] + '?callback=?';
 
   $.getJSON(urlUs, function getUserData(userData) {
     console.assert(users, 'users not given.');
+    console.log('user api called', userData);
     var displayName = userData.display_name;
+    var name = userData.name;
     var logo = userData.logo;
     var url = userData._links.self;
-    var bio = userData.bio;
-    console.log(userData);
+    var userBio = userData.bio;
+    var bio = (userBio !== null) ? userBio : '';
+
 
     $.getJSON(urlSt, function getStreamsData(streamsData) {
-      for (i in streamsData) {
-        if (streamsData.streams !== null) {
-          var game = streamsData.streams[i].channel.game;
-          var gameTitle = (streamsData.streams[i] !== null) ? game : '';
-          var status = (data.streams[i] === null) ? "mdl-color--blue-grey-50" : "mdl-color-text--purple";
-          console.log(streamsData);
+      console.log('stream api called', streamsData);
+      for (streams in streamsData) {
+        if (streamsData.streams[i] === null) {
+          var status = "mdl-color--blue-grey-50";
+        } else {
+        //  var gameTitle = streamsData.streams[i].channel.game;
+          var status = "mdl-color-text--purple";
         }
+        console.log('stream  data touched');
       }
 
-      twitch = '';
-      twitch += '<li class="mdl-list__item mdl-list__item--three-line"><a href="' + url[i] + '"<span class="mdl-list__item-primary-content"><i src="' + logo[i] + '" class="material-icons mdl-list__item-avatar"></i></a>';
-      twitch += '<span>' + displayName[i] + '</span><span class="mdl-list__item-text-body"><p>' + bio[i] + '</p><p class="gameTitle">' + gameTitle[i] + '</p>';
-      twitch += '"</span></span><span class="mdl-list__item-secondary-content"><a class="mdl-list__item-secondary-action" href="' + url[i] + '"></a><i id="star"  class="material-icons' + status[i] + '">star</i></span></li>"';
-    })
-    var content = document.getElementById('list');
-    content.innerHTML = twitch;
 
-  });
+      twitch += '<li id="' + displayName + '" class="mdl-list__item mdl-list__item--three-line">';
+      twitch += '<a href="http://www.twitch.tv/' + name + '"><span class="mdl-list__item-primary-content">';
+      twitch += '<i src="' + logo + '" class="material-icons mdl-list__item-avatar"></i>';
+      twitch += '<span>' + displayName + '</span></a>';
+      twitch += '<span class="mdl-list__item-text-body"><p>' + bio + '</p>';
+      twitch += '<p class="gameTitle">' + gameTitle + '</p></span></span>';
+      twitch += '<span class="mdl-list__item-secondary-content">';
+      twitch += '<a class="mdl-list__item-secondary-action" href="http://www.twitch.tv/' + name + '"></a>';
+      twitch += '<i id="star" class="material-icons' + status + '"></i></span></li>';
+
+      var content = document.getElementById('list');
+      content.innerHTML = twitch;
+    })
+  })
 }
